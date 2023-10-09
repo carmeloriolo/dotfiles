184f21e7ddf19eab22a6fcd39f4af39ab054edb6
Carmelo Riolo <carmelo.riolo@adevinta.com>
2023-10-09 19:26:16 +0200

    removed .scripts

--- .scripts/tmuxwrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# send-keys "scm" C-m\; \
-
-tmux new -s $1 \; \
-  split-window -h \; \
-  split-window -v \; \
--- .scripts/tunn-kill.sh
+++
@@ -1,1 +0,0 @@
-for i in $(ps -ax | grep Tunnel | grep -v grep | awk -F" " '{ print $1 }'); do kill -9 $i; done;
